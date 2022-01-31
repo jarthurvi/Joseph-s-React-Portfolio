447df530d36ed4ce3a37e59fdfdb1662ac319346
Rastar Music <rastarmusic@MacBook-Pro.local>
2022-01-31 00:12:12 -0500

    finishing

---
+++ 01-Ins_Create-React-App/classapp/src/components/pages/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p>
+        Here is where my resume goes.
+      </p>
+    </div>
+  );
+}
